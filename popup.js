eaffd63c20234460e78a17b2de935a59c542fafd
Mohammad Arafath <we.mohammad@gmail.com>
2014-11-30 00:18:27 +0530

    adding google tracker

--- popup.js
+++ popup.js
@@ -3,16 +3,30 @@ var the_code;
 function hello() {
 	the_code = document.getElementById('scriptArea').value;
 
-	chrome.tabs.executeScript(
-		{
-			file: 'jquery.js'
-		},
+	chrome.tabs.executeScript( {file: "jquery.js"}, function(){
+		chrome.tabs.executeScript({file: "track.js"}, function(){
+			chrome.tabs.executeScript({code: the_code
+			});
+		});
+	});
+
+	// chrome.tabs.executeScript(
+	// 	{
+	// 		file: 'jquery.js'
+	// 	},
 		
-		function (argument) {
-			chrome.tabs.executeScript({
-				code: the_code
-			}); 
-		}); 
+	// 	function (argument) {
+	// 		chrome.tabs.executeScript(
+	// 			{
+	// 				file: 'track.js'
+	// 			},
+
+	// 			function (argument) {
+	// 				chrome.tabs.executeScript({
+	// 						code: the_code
+	// 			});
+	// 		}); 
+	// 	}); 
 
 }
  
