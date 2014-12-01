cf531306ea7a7a12a81a5615e1542b2650533e96
Mohammad Arafath <we.mohammad@gmail.com>
2014-12-02 03:24:27 +0530

    fully functional stuff

--- popup.js
+++ popup.js
@@ -1,7 +1,6 @@
 var the_code;
 
-window.onload = function(){
-
+function ActivateEditor () {
 	BehaveHooks.add(['keydown'], function(data){
 		var numLines = data.lines.total,
 			fontSize = parseInt( getComputedStyle(data.editor.element)['font-size'] ),
@@ -19,11 +18,154 @@ window.onload = function(){
 		autoStrip: 	true,
 		autoIndent: 	true
 	});
-	
+}
+
+function Script_Editor_Cancel () {
+	$(".script-editor").remove();
 };
 
-// Fucntion generates Script Generator
+function Script_Editor_Save () {
+
+	if ( $(".form-control").val().length > 0 && $("#scriptArea").val().length >0 ) {
+		var unique_ID = Math.round(+new Date()/1000);		
+
+		var insert_Position = $(this).attr("position");
+		// alert("clicked Save");
+		var ScriptObject = {};
+		ScriptObject["name"] = $(".form-control").val();
+		ScriptObject["script"] = $("#scriptArea").val();
+
+		chrome.storage.local.get(null,function (obj){
+			console.log(JSON.stringify(obj));
+			console.log(obj);
+			if(obj.expressions == undefined){
+				obj.expressions = {};
+			}
+			if( insert_Position != "new" ){
+				obj.expressions[insert_Position] =ScriptObject;
+			}else{
+				obj.expressions[unique_ID]=ScriptObject;
+				Generate_DashBoard_Entry(ScriptObject["name"], unique_ID);
+			}
+			
+			document.getElementById('storageArea').value = JSON.stringify(obj);
+			chrome.storage.local.set(obj,function (){
+				console.log(ScriptObject);
+				console.log("Storage Hogayo");
+			});
+		});
+		Script_Editor_Cancel();		
+	}else{
+		var error_Script_Editor = document.createElement("div");
+		error_Script_Editor.setAttribute("class", "alert-stuff alert alert-danger");
+		error_Script_Editor.setAttribute("id", "error-script-alert");
+
+		if( $(".form-control").val().length == 0 && $("#scriptArea").val().length ==0){
+			error_Script_Editor.innerHTML = "<strong>Darn it!</strong> missing script a name, content";
+		}else if($(".form-control").val().length ==0){
+			error_Script_Editor.innerHTML = "<strong>Darn it!</strong> give script a name";
+		}else if ( $("#scriptArea").val().length == 0 ) {
+			error_Script_Editor.innerHTML = "<strong>Darn it!</strong> cannot save empty script";
+		};
+
+		$('.script-editor').prepend(error_Script_Editor);
+		setTimeout(function (argument) {
+			$('#error-script-alert').remove()
+		}, 3000);		
+	}
+
+};
+function Script_Editor_Reset (argument) {
+	$(".form-control").val("");
+	$("#scriptArea").val("");
+};
+
+function Unit_Expression_Run (data) {
+	var Script_Location_Key = $(this).attr('id').split('run_exp_')[1];
+	console.log(Script_Location_Key);
+	var code_2_Run ;
+	chrome.storage.local.get(null,function (obj){
+		console.log(obj);
+		code_2_Run = obj.expressions[ Script_Location_Key ]["script"];
+
+		chrome.tabs.executeScript( {file: "jquery.js"}, function(){
+			chrome.tabs.executeScript({file: "track.js"}, function(){
+				chrome.tabs.executeScript({code: code_2_Run
+				});
+			});
+		});		
+
+	});	
+};
+
+function Unit_Expression_Remove (data) {
+	console.log( "removing "+"#unit_exp_"+$(this).attr('id').split('remove_exp_')[1]);
+	var Script_Location_Key = $(this).attr('id').split('remove_exp_')[1];
+	$("#unit_exp_"+$(this).attr('id').split('remove_exp_')[1]).remove();
+	//pass new chrome set storage api after deleting and remove all the dashboard items/recreate them
+	chrome.storage.local.get(null,function (obj){
+		console.log(JSON.stringify(obj));
+		console.log(obj);
+		delete obj.expressions[Script_Location_Key]
+		document.getElementById('storageArea').value = JSON.stringify(obj);
+		chrome.storage.local.set(obj,function (){
+			console.log("Storage Hogayo");
+		});
+	});	
+};
+
+window.onload = function(){
+	ActivateEditor();
+};
+
+//Function generates new entry on dashboard
+function Generate_DashBoard_Entry ( Script_Name, serial_number ) {
+	var Unit_Expression_Container = document.createElement("div");
+	Unit_Expression_Container.setAttribute("class", "unit-exp");
+	Unit_Expression_Container.setAttribute("id","unit_exp_"+serial_number);
+
+	var Unit_Expression_Label_Container = document.createElement("span");
+	Unit_Expression_Label_Container.setAttribute("class", "label label-default");
+	Unit_Expression_Label_Container.innerText = Script_Name;
+
+	var Unit_Expression_Button_Container = document.createElement("p");
+
+	var Unit_Expression_Button_Remove_Container = document.createElement("button");
+	Unit_Expression_Button_Remove_Container.setAttribute("class", "action-button pull-right btn btn-xs btn-danger");
+	Unit_Expression_Button_Remove_Container.setAttribute("id","remove_exp_"+serial_number);
+	Unit_Expression_Button_Remove_Container.innerHTML = "&#9747";
+
+	var Unit_Expression_Button_Edit_Container = document.createElement("button");
+	Unit_Expression_Button_Edit_Container.setAttribute("class", "action-button pull-right btn btn-xs btn-primary");
+	Unit_Expression_Button_Edit_Container.setAttribute("id", "edit_exp_"+serial_number);
+	Unit_Expression_Button_Edit_Container.setAttribute("position", serial_number);
+	Unit_Expression_Button_Edit_Container.innerHTML = "&#9776";
+
+	var Unit_Expression_Button_Run_Container = document.createElement("button");
+	Unit_Expression_Button_Run_Container.setAttribute("class", "action-button pull-right btn btn-xs btn-success");
+	Unit_Expression_Button_Run_Container.setAttribute("id", "run_exp_"+serial_number);
+	Unit_Expression_Button_Run_Container.innerHTML = "&#8635";
+
+	//add to button
+	Unit_Expression_Button_Container.appendChild(Unit_Expression_Button_Remove_Container);
+	Unit_Expression_Button_Container.appendChild(Unit_Expression_Button_Edit_Container);
+	Unit_Expression_Button_Container.appendChild(Unit_Expression_Button_Run_Container);
+
+	Unit_Expression_Container.appendChild(Unit_Expression_Label_Container);
+	Unit_Expression_Container.appendChild(Unit_Expression_Button_Container);
+
+	document.getElementsByClassName('dashboard-exp')[0].appendChild(Unit_Expression_Container);
+
+	//calling neccesary event listeners on these
+	document.getElementById("run_exp_"+serial_number).addEventListener('click', Unit_Expression_Run);
+	document.getElementById("edit_exp_"+serial_number).addEventListener('click', Generate_Script_Editor);
+	document.getElementById("remove_exp_"+serial_number).addEventListener('click', Unit_Expression_Remove);
+
+}
+
+//Fucntion generates Script Generator
 function Generate_Script_Editor ( data ) {
+	var position_creation = $(this).attr("position");
 
 	var script_Editor_Container = document.createElement("div");
 	script_Editor_Container.setAttribute("class", "script-editor");
@@ -39,6 +181,7 @@ function Generate_Script_Editor ( data ) {
 	var script_Editor_Title_Name_Container = document.createElement("input");
 	script_Editor_Title_Name_Container.setAttribute("class","form-control");
 	script_Editor_Title_Name_Container.setAttribute("type", "text");
+	script_Editor_Title_Name_Container.setAttribute("maxlength", 20);
 	script_Editor_Title_Name_Container.setAttribute("placeholder", "Script Name");
 
 	script_Editor_Title_Container.appendChild(script_Editor_Title_addon_Container);
@@ -63,6 +206,7 @@ function Generate_Script_Editor ( data ) {
 	var script_Editor_Save_Action_Container = document.createElement("button");
 	script_Editor_Save_Action_Container.setAttribute("class", "btn btn-sm btn-success");
 	script_Editor_Save_Action_Container.setAttribute("id", "Script_Editor_Save");
+	script_Editor_Save_Action_Container.setAttribute("position", "new");
 	script_Editor_Save_Action_Container.innerText = "Save";
 
 	var script_Editor_Cancel_Action_Container = document.createElement("button");
@@ -85,23 +229,24 @@ function Generate_Script_Editor ( data ) {
 	script_Editor_Container.appendChild(script_Editor_Action_Container);
 
 	document.body.appendChild(script_Editor_Container);
-
+	chrome.storage.local.get(null,function (obj){		
+		
+		if( position_creation != "new" ){
+			$(".form-control").val(obj.expressions[ position_creation ]["name"]);
+			$("#scriptArea").val(obj.expressions[ position_creation ]["script"]);
+			$("#Script_Editor_Save").attr("position", position_creation );
+		}
+	});
 	//initialize button functions
-	initialize_Editor_Button_Actions();
+	initialize_Editor_Button_Actions(data);
+	
 }
 
 // functions for save, cancel, reset on script editor
-function initialize_Editor_Button_Actions(){
-	function Script_Editor_Cancel () {
-		$(".script-editor").remove();
-	};
-	function Script_Editor_Save (argument) {
-		alert("clicked Save");
-	};
-	function Script_Editor_Reset (argument) {
-		$(".form-control").val("");
-		$("#scriptArea").val("");	
-	};
+function initialize_Editor_Button_Actions(data){
+
+	ActivateEditor();
+	
 	document.getElementById('Script_Editor_Save').addEventListener('click', Script_Editor_Save);
 	document.getElementById('Script_Editor_Reset').addEventListener('click', Script_Editor_Reset);
 	document.getElementById('Script_Editor_Cancel').addEventListener('click', Script_Editor_Cancel);
@@ -141,10 +286,22 @@ document.addEventListener("DOMContentLoaded",function (){
     chrome.storage.local.get(null,function (obj){
         console.log(JSON.stringify(obj));
         document.getElementById('storageArea').value = JSON.stringify(obj);
+        //call function to load all DB scripts
+        $('.dashboard-exp').empty();
+        for( var ScriptObject in obj.expressions){
+        	Generate_DashBoard_Entry( obj.expressions[ScriptObject]["name"], ScriptObject )
+        }
         console.log("Storage Aagayo");
     });
 
 });
 
-document.getElementById('clickme').addEventListener('click', hello);
+function clearChromeStorage () {
+    chrome.storage.local.clear(function (){
+        console.log("Storage Nikalgaya");
+    });
+    // Math.round(+new Date()/1000);
+}
+
 document.getElementById('AddNewScript').addEventListener('click', Generate_Script_Editor);
+document.getElementById('clearStorage').addEventListener('click', clearChromeStorage);
